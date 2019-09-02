2da7b89079ed656027df7114a380f9d47e64157c
danielkroning <54662401+danielkroning@users.noreply.github.com>
2019-09-02 19:30:45 +0200

    Final Working Version

--- Pygame.py
+++ Pygame.py
@@ -4,8 +4,9 @@ from dataclasses import dataclass
 import random
 
 auflösung = 1000
-raster = 10
+raster = int(input("Please enter the size of your Game: "))
 abstand = auflösung // raster
+anzMinen = int(input("Please enter the number of mines: "))
 
 pygame.init()
 screen = pygame.display.set_mode([auflösung, auflösung])
@@ -25,8 +26,9 @@ for n in range(9):
 #while True:
  #   pygame.display.flip()
 
-anzMinen = 10
+
 matrix = []
+benachbarteFelder = [(-1,-1),(-1,0),(-1,+1),(0,-1),(0,+1),(+1,-1),(+1,0),(+1,+1)]
 
 @dataclass
 class Zelle():
@@ -49,16 +51,64 @@ class Zelle():
                 screen.blit(cell_marked, pos)
             else:
                 screen.blit(cell_normal, pos)
+    
+    def anzahlMinenErmitteln(self):
+        for pos in benachbarteFelder:
+            neueZeile = self.zeile + pos[0]
+            neueSpalte = self.spalte + pos[1]
+            if neueZeile >= 0 and neueZeile < raster and neueSpalte >= 0 and neueSpalte < raster:
+                if matrix[neueZeile*raster+neueSpalte].mine:
+                    self.anzMinenDrumrum+=1
+
+def floodFill(zeile, spalte):
+    for pos in benachbarteFelder:
+        neueZeile = zeile + pos[0]
+        neueSpalte = spalte + pos[1]
+        if neueZeile >= 0 and neueZeile < raster and neueSpalte >= 0 and neueSpalte < raster:
+            cell = matrix[neueZeile*raster+neueSpalte]
+            if cell.anzMinenDrumrum == 0 and not cell.selected:
+                cell.selected = True
+                floodFill(neueZeile, neueSpalte)
+            else: 
+                cell.selected = True
+
 
 for n in range(raster*raster):
     matrix.append(Zelle(n // raster, n % raster))
+
+while anzMinen > 0:
+    x = random.randrange(raster*raster)
+    if not matrix[x].mine:
+        matrix[x].mine = True
+        anzMinen-=1
+
+for objekt in matrix:
+    objekt.anzahlMinenErmitteln()
+
 
 weitermachen = True
 while weitermachen:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             weitermachen = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouseX, mouseY = pygame.mouse.get_pos()
+            spalte = mouseX // abstand
+            zeile = mouseY // abstand
+            i = zeile*raster+spalte
+            cell = matrix[i]
+            if pygame.mouse.get_pressed()[2]:
+                cell.flagged = not cell.flagged
+            if pygame.mouse.get_pressed()[0]:
+                cell.selected = True
+                if cell.anzMinenDrumrum == 0 and not cell.mine:
+                    floodFill(zeile, spalte)
+                if cell.mine:
+                    for objekt in matrix:
+                        objekt.selected = True
+
     for objekt in matrix:
+        #objekt.selected = True
         objekt.show()
     pygame.display.flip()
 
