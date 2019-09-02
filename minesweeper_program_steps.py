fb1f8150318efd16b4c4486678daf9ea7f2ab656
ALorenzl <alorenzl@gmx.de>
2019-09-02 10:35:38 +0200

    Create minesweeper_program_steps.py
    Different steps in programming minesweeper

---
+++ minesweeper_program_steps.py
@@ -0,0 +1,25 @@
+'''
+Steps for creating Minesweeper:
+
+1. Create playground
+1.1 Create a grid (user input)
+1.2 Define number of bombs (user input // maybe: with limits)
+1.3 Randomly distrubute bombs over the grid
+1.4 Create a list with the positions of the bombs [row][column]
+1.5 Define a funciton which adds 1 to every field next to a bomb (use list from 1.4) if there is no other bomb
+        --> This forms the solution_grid
+1.6 Create an "empty_grid" which is available to the user with the same dimnesions (using '-')
+
+2. Rules
+2.1 Game ends as soon as you hit a mine
+2.2 Game proceeds if you hit a field with a number > 0 
+2.3 If you hit a 0 all 8 sourrounding fields are revealed
+
+3. Gameplay
+3.1 Flags putting in using '?' (right-click)
+3.2 Running time
+3.3 Being able to click on the single fields to reveal them (left-click)
+3.4 Scorekeeper
+3.5 Give the user 3 options (Easy, Middle and Hard)
+
+'''
